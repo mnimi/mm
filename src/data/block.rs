baa24c9823f32e20d03d6160f45f928ef4d75fb5
OsmiumShield <oswitten@gmail.com>
2021-01-04 22:00:35 -0600

    started working on shared functionality for blocks.

--- src/data/block.rs
+++ src/data/block.rs
@@ -1,28 +1,45 @@
+use serde::{Deserialize, Serialize};
+
 #[derive(Clone, Deserialize, Serialize)]
-pub struct Block<'a>
+pub struct Block
 {
   id: u32,
+  hardness : u32,
   tag: Vec<u32>,
-  block_type: Vec<&'a dyn BlockType>,
 }
 
-pub trait BlockType{
+pub trait SmartBlock
+{
+  fn update(self);
+}
 
-  fn update();
+struct SBHandler<'a>{
+  chunk : &'a mut Chunk
+}
 
+#[derive(Copy, Clone, Serialize, Deserialize)]
+pub struct Liquid
+{
+  source_distance : u32
+}
+impl SmartBlock for Liquid
+{
+  fn update(self){
+
+  }
 }
 
 #[derive(Clone, Deserialize, Serialize)]
 pub struct Chunk
 {
-  block: Vec<Block>
+  block : Vec<Block>,
 }
 
 impl Chunk
 {
-  pub fn index(x: u8, y: u8, z: u8) -> u32
+  pub fn index(x: u32, y: u8, z: u8) -> u32
   {
-    (x + (y * 16) + (z * 256)) as u32
+    (x + (y * 16) + (z * 256))
   }
 }
 
@@ -40,8 +57,8 @@ impl Map
   pub fn get_chunk(&self, x: i32, y: i32, z: i32) -> Chunk
   {
     let x: i32 = (x - (x % 16)) / 16;
-    let y: i32 = (x - (x % 16)) / 16;
-    let z: i32 = (x - (x % 16)) / 16;
+    let y: i32 = (y - (y % 16)) / 16;
+    let z: i32 = (z - (z % 16)) / 16;
 
     static mut VALUE: Option<Chunk> = None;
 
@@ -69,6 +86,10 @@ impl Map
 
   fn load_chunk(x: i32, y: i32, z: i32) -> Chunk
   {
+    todo!()
+  }
+
+  fn unload_chunk(x: i32, y: i32, z: i32){
     todo!()
   }
 }
